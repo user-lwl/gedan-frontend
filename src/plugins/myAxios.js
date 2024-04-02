4fc4f85f4e0ce7193786aacdaf3685b47d5629c8
user-lwl <1399097219@qq.com>
2024-04-03 00:12:03 +0800

    歌单前台

--- src/plugins/myAxios.js
+++ src/plugins/myAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 创建一个 axios 实例
 const axiosInstance = axios.create({
-    baseURL: 'http://60.205.182.243:8101/api', // 设置你的自定义 baseURL
+    baseURL: 'http://gedan-backend.douyoubanfan.icu/api', // 设置你的自定义 baseURL
 });
 
 export default axiosInstance;
